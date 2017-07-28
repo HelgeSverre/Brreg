f6ef05a8191cd2598d5b2d6cf71820a6c79dbdfd
Helge Sverre Hessevik Liseth <helge.sverre@gmail.com>
2017-07-28 16:02:41 +0200

    Update BrregCompanyDataTransformer.php

--- src/Transformers/BrregCompanyDataTransformer.php
+++ src/Transformers/BrregCompanyDataTransformer.php
@@ -13,17 +13,62 @@ class BrregCompanyDataTransformer extends TransformerAbstract
         return [
             "name" => $company->navn,
             "registration_number" => $company->organisasjonsnummer,
+            "registration_number_pretty" => implode(" ", str_split($company->organisasjonsnummer, 3)),
             "registration_date" => $company->registreringsdatoEnhetsregisteret,
             "employee_count" => $company->antallAnsatte,
-            "address" => (isset($company->forretningsadresse) ? [
-                "address" => $company->forretningsadresse->adresse,
-                "postal_code" => $company->forretningsadresse->postnummer,
-                "city" => $company->forretningsadresse->poststed,
-                "country" => $company->forretningsadresse->land,
-            ] : null),
-            "bankrupt" => $company->konkurs == "N" ? 0 : 1
+            "visiting_address" => (isset($company->forretningsadresse)) ? $this->parseAddress($company->forretningsadresse) : null,
+            "mailing_address" => (isset($company->postadresse)) ? $this->parseAddress($company->postadresse) : null,
+            "bankrupt" => $company->konkurs == "N" ? 0 : 1,
+            "business_sectors" => $this->parseBusinessSector($company),
+            "business_type" => $company->organisasjonsform
         ];
 
     }
 
+    private function parseAddress($address)
+    {
+        // No address
+        if (!$address) {
+            return null;
+        }
+
+        $addressInfo = [];
+
+
+        if (isset($address->adresse)) {
+            $addressInfo["address"] = $address->adresse;
+        }
+
+        if (isset($address->postnummer)) {
+            $addressInfo["postal_code"] = $address->postnummer;
+        }
+
+        if (isset($address->poststed)) {
+            $addressInfo["city"] = $address->poststed;
+        }
+
+        if (isset($address->land)) {
+            $addressInfo["country"] = $address->land;
+        }
+
+        return $addressInfo;
+    }
+
+    private function parseBusinessSector($company)
+    {
+        $sectors = [];
+
+        // There can be up to 3 business sectors defined afaik
+        for ($i = 1; $i <= 3; $i++) {
+            $field = "naeringskode{$i}";
+
+            if (!isset($company->$field)) {
+                break;
+            }
+
+            $sectors[] = $company->$field->kode . " - " . $company->$field->beskrivelse;
+        }
+
+        return $sectors;
+    }
 }
