aed11d39d3fc757cff029d9546e303f591e59efb
Helge Sverre <helge.sverre@gmail.com>
2020-01-02 18:00:38 +0100

    Use array

--- src/Transformers/BrregCompanyDataTransformer.php
+++ src/Transformers/BrregCompanyDataTransformer.php
@@ -11,18 +11,18 @@ class BrregCompanyDataTransformer extends TransformerAbstract
     public function transform($company)
     {
         return [
-            "name" => $company->navn,
-            "registration_number" => $company->organisasjonsnummer,
-            "registration_number_pretty" => implode(" ", str_split($company->organisasjonsnummer, 3)),
-            "registration_date" => $company->registreringsdatoEnhetsregisteret,
-            "employee_count" => $company->antallAnsatte,
-            "visiting_address" => (isset($company->forretningsadresse)) ? $this->parseAddress($company->forretningsadresse) : null,
-            "mailing_address" => (isset($company->postadresse)) ? $this->parseAddress($company->postadresse) : null,
-            "bankrupt" => $company->konkurs == "N" ? 0 : 1,
-            "vat_registered" => $company->registrertIMvaregisteret == "J" ? 1 : 0,
+            "name" => $company["navn"],
+            "registration_number" => $company["organisasjonsnummer"],
+            "registration_number_pretty" => implode(" ", str_split($company["organisasjonsnummer"], 3)),
+            "registration_date" => $company["registreringsdatoEnhetsregisteret"],
+            "employee_count" => $company["antallAnsatte"],
+            "visiting_address" => (isset($company["forretningsadresse"])) ? $this->parseAddress($company["forretningsadresse"]) : null,
+            "mailing_address" => (isset($company["postadresse"])) ? $this->parseAddress($company["postadresse"]) : null,
+            "bankrupt" => $company["konkurs"] == "N" ? 0 : 1,
+            "vat_registered" => $company["registrertIMvaregisteret"] == "J" ? 1 : 0,
             "business_sectors" => $this->parseBusinessSector($company),
-            "business_type" => $company->organisasjonsform,
-            "website" => isset($company->hjemmeside) ? $company->hjemmeside : null
+            "business_type" => $company["organisasjonsform"],
+            "website" => isset($company["hjemmeside"]) ? $company["hjemmeside"] : null
         ];
 
     }
@@ -37,20 +37,20 @@ class BrregCompanyDataTransformer extends TransformerAbstract
         $addressInfo = [];
 
 
-        if (isset($address->adresse)) {
-            $addressInfo["address"] = $address->adresse;
+        if (isset($address["adresse"])) {
+            $addressInfo["address"] = $address["adresse"];
         }
 
-        if (isset($address->postnummer)) {
-            $addressInfo["postal_code"] = $address->postnummer;
+        if (isset($address["postnummer"])) {
+            $addressInfo["postal_code"] = $address["postnummer"];
         }
 
-        if (isset($address->poststed)) {
-            $addressInfo["city"] = $address->poststed;
+        if (isset($address["poststed"])) {
+            $addressInfo["city"] = $address["poststed"];
         }
 
-        if (isset($address->land)) {
-            $addressInfo["country"] = $address->land;
+        if (isset($address["land"])) {
+            $addressInfo["country"] = $address["land"];
         }
 
         return $addressInfo;
@@ -64,11 +64,11 @@ class BrregCompanyDataTransformer extends TransformerAbstract
         for ($i = 1; $i <= 3; $i++) {
             $field = "naeringskode{$i}";
 
-            if (!isset($company->$field)) {
+            if (!isset($company[$field])) {
                 break;
             }
 
-            $sectors[] = $company->$field->kode . " - " . $company->$field->beskrivelse;
+            $sectors[] = $company[$field]["kode"] . " - " . $company[$field]["beskrivelse"];
         }
 
         return $sectors;
